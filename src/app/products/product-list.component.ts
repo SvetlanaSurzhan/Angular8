a2b569555997674b4ca560f40d6ffb662518e5d3
SvetlanaSurzhan <svetlana.d.surzhan@gmail.com>
2020-07-29 13:53:28 -0400

    created ProductListComponent properties

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -7,5 +7,27 @@ import { templateJitUrl } from '@angular/compiler';
     templateUrl: './product-list.component.html'
 })
 export class ProductListComponent {
-
+    pageTitle: string = 'Product list for Lana';
+    products: any[]=[
+        {
+            "productId": 1,
+            "productName": "Leaf Rake",
+            "productCode": "GDN-0011",
+            "releaseDate": "March 19, 2019",
+            "description": "Leaf rake with 48-inch wooden handle.",
+            "price": 19.95,
+            "starRating": 3.2,
+            "imageUrl": "assets/images/leaf_rake.png"
+        },
+        {
+            "productId": 10,
+            "productName": "Video Game Controller",
+            "productCode": "GMG-0042",
+            "releaseDate": "October 15, 2018",
+            "description": "Standard two-button video game controller",
+            "price": 35.95,
+            "starRating": 4.6,
+            "imageUrl": "assets/images/xbox-controller.png"
+        }
+    ];
 }
