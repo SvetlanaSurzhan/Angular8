944c110b899b88600cd2478ebf64751dd03887cd
Svetlana Surzhan <svetlana.d.surzhan>
2020-08-03 16:29:00 -0400

    added filtering logic into ProductListComponent

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -14,7 +14,20 @@ export class ProductListComponent implements OnInit {
     imageMargin: number = 2;
     showImage: boolean = false;
     showName: boolean = false;
-    listFilter: string = "cart";
+
+    _listFilter: string;
+    get listFilter() : string {
+        return this._listFilter;
+    }
+    
+    set listFilter(value : string) {
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+    
+    
+
+    filteredProducts: IProduct[];
     products: IProduct[]=[
         {
             "productId": 1,
@@ -37,6 +50,17 @@ export class ProductListComponent implements OnInit {
             "imageUrl": "assets/images/xbox-controller.png"
         }
     ];
+
+    constructor() {
+        this.filteredProducts = this.products;
+        this.listFilter = "cart";
+    }
+    
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) =>
+            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
 
     toggleImage(): void {
         this.showImage = !this.showImage;
