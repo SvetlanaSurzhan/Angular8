dd46402c4908cc3903ff67e6150751f0073388cf
Svetlana Surzhan <svetlana.d.surzhan>
2020-08-04 12:12:59 -0400

    added products from products.json

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -16,19 +16,19 @@ export class ProductListComponent implements OnInit {
     showName: boolean = false;
 
     _listFilter: string;
-    get listFilter() : string {
+    get listFilter(): string {
         return this._listFilter;
     }
-    
-    set listFilter(value : string) {
+
+    set listFilter(value: string) {
         this._listFilter = value;
         this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
     }
-    
-    
+
+
 
     filteredProducts: IProduct[];
-    products: IProduct[]=[
+    products: IProduct[] = [
         {
             "productId": 1,
             "productName": "Leaf Rake",
@@ -38,6 +38,36 @@ export class ProductListComponent implements OnInit {
             "price": 19.9576,
             "starRating": 3.2,
             "imageUrl": "assets/images/leaf_rake.png"
+        },
+        {
+            "productId": 2,
+            "productName": "Garden Cart",
+            "productCode": "GDN-0023",
+            "releaseDate": "March 18, 2019",
+            "description": "15 gallon capacity rolling garden cart",
+            "price": 32.99,
+            "starRating": 4.2,
+            "imageUrl": "assets/images/garden_cart.png"
+        },
+        {
+            "productId": 5,
+            "productName": "Hammer",
+            "productCode": "TBX-0048",
+            "releaseDate": "May 21, 2019",
+            "description": "Curved claw steel hammer",
+            "price": 8.9,
+            "starRating": 4.8,
+            "imageUrl": "assets/images/hammer.png"
+        },
+        {
+            "productId": 8,
+            "productName": "Saw",
+            "productCode": "TBX-0022",
+            "releaseDate": "May 15, 2019",
+            "description": "15-inch steel blade hand saw",
+            "price": 11.55,
+            "starRating": 3.7,
+            "imageUrl": "assets/images/saw.png"
         },
         {
             "productId": 10,
@@ -55,7 +85,7 @@ export class ProductListComponent implements OnInit {
         this.filteredProducts = this.products;
         this.listFilter = "cart";
     }
-    
+
     performFilter(filterBy: string): IProduct[] {
         filterBy = filterBy.toLocaleLowerCase();
         return this.products.filter((product: IProduct) =>
@@ -65,7 +95,7 @@ export class ProductListComponent implements OnInit {
     toggleImage(): void {
         this.showImage = !this.showImage;
     }
-    togglePageTitle():void{
+    togglePageTitle(): void {
         this.showName = !this.showName;
     }
     ngOnInit(): void {
