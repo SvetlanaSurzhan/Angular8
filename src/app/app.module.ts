6137077454fc418eb6760223be7fa1aedae0b2c4
Svetlana Surzhan <svetlana.d.surzhan>
2020-08-31 17:36:15 -0400

    tied and placed routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,7 +29,7 @@ import { WelcomeComponent } from './home/welcome.component';
     RouterModule.forRoot([
       { path: "products", component: ProductListComponent },
       { path: "products/:id", component: ProductDetailComponent },
-      { path: "welcome", component: ProductListComponent },
+      { path: "welcome", component: WelcomeComponent },
       { path: "", redirectTo: "welcome", pathMatch: "full" },
       { path: "**", redirectTo: "welcome", pathMatch: "full" }
     ], {useHash: true})
